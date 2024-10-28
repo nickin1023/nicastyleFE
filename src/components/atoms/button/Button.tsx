3ca3ff08879508023499dd93aa3eac9c014ebf1d
nickin1023 <icape0000@gmail.com>
2024-10-28 11:24:14 +0900

    make simple button

---
+++ src/components/atoms/button/Button.tsx
@@ -0,0 +1,44 @@
+import React from "react";
+import { VariantProps, cva } from "class-variance-authority";
+import { cn } from "@/utils/cn";
+
+export const buttonVariants = cva(
+  `flex h-10 items-center justify-center gap-2 rounded-full px-4 text-textL_medium transition disabled:opacity-50 disabled:pointer-events-none transition`,
+  {
+    variants: {
+      variant: {
+        primary: `bg-accent text-primaryWhite hover:opacity-75`,
+        outline: `bg-blue-500 text-red-300 border border-accent hover:opacity-75`,
+        icon: `bg-transparent hover:bg-bg-gray rounded-full p-2 h-fit`,
+      },
+    },
+    defaultVariants: {
+      variant: "primary",
+    },
+  }
+);
+
+interface ButtonProps
+  extends React.ButtonHTMLAttributes<HTMLButtonElement>,
+    VariantProps<typeof buttonVariants> {}
+
+export const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
+  (
+    { className, variant, children, disabled, type = "button", ...props },
+    ref
+  ) => {
+    return (
+      <button
+        type={type}
+        className={cn(buttonVariants({ variant, className }))}
+        ref={ref}
+        disabled={disabled}
+        {...props}
+      >
+        {children}
+      </button>
+    );
+  }
+);
+
+Button.displayName = "Button";
---
+++ src/utils/cn.ts
@@ -0,0 +1,5 @@
+import clsx from "clsx";
+
+export const cn = (name: String) => {
+  return clsx(name);
+};
