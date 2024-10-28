ee770b223f7d2c177711d301d6e78fcafc1cf2ce
nickin1023 <icape0000@gmail.com>
2024-10-28 11:25:40 +0900

    make common layout frame

---
+++ src/components/organisms/header/Header.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export const Header = () => {
+  return (
+    <>
+      <header className="w-full bg-black text-white">
+        <p>header</p>
+      </header>
+    </>
+  );
+};
+
+Header.displayName = "Header";
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,14 @@
+import { Header } from "@/components/organisms/header/Header";
+import { Layout } from "@/components/templates";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </>
+  );
 }
