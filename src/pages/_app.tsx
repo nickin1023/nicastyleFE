0e8643f47d294e768db56a608ca18f1ed09d8bc8
nickin1023 <icape0000@gmail.com>
2024-10-29 13:24:42 +0900

    add title to head

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,14 @@
-import { Header } from "@/components/organisms/header/Header";
 import { Layout } from "@/components/templates";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
+      <Head>
+        <title>nica style</title>
+      </Head>
       <Layout>
         <Component {...pageProps} />
       </Layout>
