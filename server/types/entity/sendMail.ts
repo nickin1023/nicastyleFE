909937c6b6dea465cb6aac84ea89d64cffd352ae
nickin1023 <icape0000@gmail.com>
2024-11-29 06:49:29 +0900

    make server communication type

---
+++ server/types/entity/sendMail.ts
@@ -0,0 +1,11 @@
+export type SendMailRequest = {
+  type: "Contact" | "Comment";
+  message: MailMessage;
+};
+
+type MailMessage = {
+  name?: string | undefined;
+  address?: string | undefined;
+  subject?: string | undefined;
+  main: string;
+};
