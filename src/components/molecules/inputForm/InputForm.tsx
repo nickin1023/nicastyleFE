ad14d00b9983da1458698b21abd297ac2ebfc4b9
nickin1023 <icape0000@gmail.com>
2025-01-12 15:52:42 +0900

    separete label component

--- src/components/molecules/inputForm/InputForm.tsx
+++ src/components/molecules/inputForm/InputForm.tsx
@@ -1,23 +1,6 @@
-import { cn } from "@/src/utils/cn";
-import { cva } from "class-variance-authority";
 import { HTMLInputTypeAttribute, forwardRef } from "react";
 import { Input } from "../../atoms/input/Input";
-
-const labelVariants = cva(
-  `flex h-10 items-center gap-2 px-4 text-textL_medium transition disabled:opacity-50 disabled:pointer-events-none transition`,
-  {
-    variants: {
-      variant: {
-        primary: `bg-gray-200 text-primaryWhite hover:opacity-75`,
-        outline: `bg-blue-500 text-red-300 border border-accent hover:opacity-75`,
-        icon: `bg-transparent hover:bg-bg-gray rounded-full p-2 h-fit`,
-      },
-    },
-    defaultVariants: {
-      variant: "primary",
-    },
-  }
-);
+import { Label } from "../../atoms/label/Label";
 
 type InputFormProps = {
   variant: "primary" | "outline" | "icon" | null | undefined;
@@ -31,9 +14,9 @@ export const InputForm = forwardRef<HTMLInputElement, InputFormProps>(
   ({ variant, formName, type, placeholder, labelName, ...props }, ref) => {
     return (
       <div className="m-5">
-        <label className={cn(labelVariants({ variant }))} htmlFor={formName}>
+        <Label variant={variant} name={formName}>
           {labelName}
-        </label>
+        </Label>
         <Input
           id={formName}
           variant={variant}
