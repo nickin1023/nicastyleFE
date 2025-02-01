35a3317d86dbaba33bc3de79f54409934b34dc7c
nickin1023 <icape0000@gmail.com>
2025-02-01 22:06:06 +0900

    make html editor

--- src/features/administrator/blogs/component/blogContent.tsx
+++ src/features/administrator/blogs/component/blogContent.tsx
@@ -1,15 +1,41 @@
 import { HtmlContent } from "@/src/components/organisms/htmlContent/HtmlContent";
+import Editor from "@monaco-editor/react";
+import { useRef } from "react";
 import { AdminContentParams } from "../types/blogContent";
 
 export const AdminBlogContent = (params: AdminContentParams) => {
-  const { html, isEdit, isPreview } = params;
+  const { html, setHtml, isEdit, isPreview } = params;
 
+  const editorRef = useRef(null);
+
+  function handleEditorDidMount(editor: any) {
+    editorRef.current = editor;
+  }
+
+  const monacoOptions = {
+    readOnly: false,
+    minimap: { enabled: false },
+    scrollbar: {
+      alwaysConsumeMouseWheel: false,
+    },
+  };
   return (
     <>
       {isEdit && !isPreview ? (
         <>
-          <div>
-            <>{html}</>
+          <p>contents</p>
+          <div className="border border-black">
+            <Editor
+              height="90vh"
+              defaultLanguage="html"
+              defaultValue={html}
+              onMount={handleEditorDidMount}
+              onChange={(newValue) => {
+                setHtml(newValue!);
+              }}
+              options={monacoOptions}
+              theme="light"
+            />
           </div>
         </>
       ) : (
