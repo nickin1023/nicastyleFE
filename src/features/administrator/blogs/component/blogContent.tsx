9b61589a0fa21c4e9ed16b2ffc34f4e32d4c6e8a
nickin1023 <icape0000@gmail.com>
2025-02-02 17:04:15 +0900

    refactor edit post

--- src/features/administrator/blogs/component/blogContent.tsx
+++ src/features/administrator/blogs/component/blogContent.tsx
@@ -4,7 +4,7 @@ import { useRef } from "react";
 import { AdminContentParams } from "../types/blogContent";
 
 export const AdminBlogContent = (params: AdminContentParams) => {
-  const { html, setHtml, isEdit, isPreview } = params;
+  const { html, setHtml, title, setTitle, isEdit, isPreview } = params;
 
   const editorRef = useRef(null);
 
@@ -23,6 +23,15 @@ export const AdminBlogContent = (params: AdminContentParams) => {
     <>
       {isEdit && !isPreview ? (
         <>
+          <div className="py-2">
+            <p>title</p>
+            <input
+              className="border border-black w-full"
+              type="text"
+              value={title}
+              onChange={(e) => setTitle(e.target.value)}
+            />
+          </div>
           <p>contents</p>
           <div className="border border-black">
             <Editor
@@ -39,7 +48,10 @@ export const AdminBlogContent = (params: AdminContentParams) => {
           </div>
         </>
       ) : (
-        <HtmlContent html={html} />
+        <>
+          <p>title: {title}</p>
+          <HtmlContent html={html} />
+        </>
       )}
     </>
   );
