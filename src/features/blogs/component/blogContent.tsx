5647b9e749135322128a74014c6944f7d65c6781
nickin1023 <icape0000@gmail.com>
2025-02-08 08:10:46 +0900

    add date info to blog content

--- src/features/blogs/component/blogContent.tsx
+++ src/features/blogs/component/blogContent.tsx
@@ -1,6 +1,22 @@
 import { HtmlContent } from "@/src/components/organisms/htmlContent/HtmlContent";
+import { isoToDotDate } from "@/src/utils/dateFormatter";
 import { ContentParams } from "../types/blogContent";
 
 export const BlogContent = (params: ContentParams) => {
-  return <HtmlContent html={params.html} />;
+  const { title, featureImageUrl, html, published_at, updated_at } = params;
+  return (
+    <>
+      <p>title: {title}</p>
+      <p className="flex justify-end">
+        <time dateTime={published_at} itemProp="datePublished">
+          公開日: {isoToDotDate(published_at)}
+        </time>
+        &ensp;
+        <time dateTime={updated_at} itemProp="dateModified">
+          最終更新日: {isoToDotDate(updated_at)}
+        </time>
+      </p>
+      <HtmlContent html={html} />
+    </>
+  );
 };
---
+++ src/utils/dateFormatter.ts
@@ -0,0 +1,10 @@
+export const isoToDotDate = (isoDateString: string): string => {
+  const date = new Date(isoDateString);
+  return date
+    .toLocaleDateString("ja-JP", {
+      year: "numeric",
+      month: "2-digit",
+      day: "2-digit",
+    })
+    .replace(/\//g, ".");
+};
