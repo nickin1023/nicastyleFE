db7d580e50d73dfd7a5714cf3d032f5f0d6f579e
nickin1023 <icape0000@gmail.com>
2025-02-09 17:15:27 +0900

    fix snackbar sending comment and contact

--- src/features/contact/component/contactForm.tsx
+++ src/features/contact/component/contactForm.tsx
@@ -6,13 +6,14 @@ import {
 import { Button } from "@/src/components/atoms/button/Button";
 import { InputForm } from "@/src/components/molecules/inputForm/InputForm";
 import { TextAreaForm } from "@/src/components/molecules/textAreaForm/TextAreaForm";
-import { KeyboardEventHandler, useState } from "react";
+import { Snackbar } from "@/src/components/organisms/snackbar/Snackbar";
+import { useSnackbar } from "@/src/hooks/useSnackbar";
+import { KeyboardEventHandler } from "react";
 import { SubmitHandler, useForm } from "react-hook-form";
 import { sendMail } from "../api/sendMail";
 
 export const ContactForm = () => {
-  const [isSnackbarVisible, setIsSnackbarVisible] = useState<boolean>(false);
-  const [snackbarMessage, setSnackbarMessage] = useState("");
+  const { isShow, message, variant, openSnackBar } = useSnackbar();
 
   const {
     register,
@@ -33,16 +34,11 @@ export const ContactForm = () => {
     };
     const res: SendMailResponse = await sendMail(mailRequest);
     if (res.result === "Success") {
-      setSnackbarMessage("送信に成功しました。");
+      openSnackBar("送信に成功しました。", "success");
       reset();
     } else {
-      setSnackbarMessage("送信に失敗しました。再度お試しください。");
+      openSnackBar("送信に失敗しました。再度お試しください。。", "warn");
     }
-
-    setIsSnackbarVisible(true);
-    setTimeout(() => {
-      setIsSnackbarVisible(false);
-    }, 3000);
   };
 
   const handleFormSubmit: KeyboardEventHandler = (e) => {
@@ -53,68 +49,66 @@ export const ContactForm = () => {
   };
 
   return (
-    <div className="relative">
-      <h1>お問い合わせ</h1>
-      <p>
-        当サイトへのご意見やお問い合わせは下記フォームよりお願いいたします。
-        <br />
-      </p>
-      {isSnackbarVisible && (
-        <div className="absolute top-0 left-0 right-0 bg-blue-500 text-black px-4 py-3 z-10">
-          <p>{snackbarMessage}</p>
-        </div>
-      )}
-      <form onKeyDown={handleFormSubmit}>
-        <InputForm
-          variant={"primary"}
-          formName="name"
-          type="text"
-          placeholder="name"
-          labelName="お名前"
-          {...register("name", {
-            required: "お名前を入力してください",
-          })}
-        />
-        {errors.name?.message && (
-          <p className="error-message">{errors.name?.message}</p>
-        )}
-        <InputForm
-          variant={"primary"}
-          formName="address"
-          type="text"
-          placeholder="address"
-          labelName="メールアドレス"
-          {...register("address", {
-            required: "アドレスを入力してください",
-          })}
-        />
-        <InputForm
-          variant={"primary"}
-          formName="subject"
-          type="text"
-          placeholder="subject"
-          labelName="タイトル"
-          {...register("subject")}
-        />
-        <TextAreaForm
-          variant={"primary"}
-          formName="main"
-          type="text"
-          labelName="本文"
-          rows={5}
-          {...register("main", {
-            required: "本文を入力してください",
-          })}
-        />
-        <Button
-          variant={"primary"}
-          className="m-5"
-          type="submit"
-          onClick={handleSubmit(onSubmit)}
-        >
-          送信
-        </Button>
-      </form>
-    </div>
+    <>
+      <Snackbar isShow={isShow} message={message} variant={variant} />
+      <div className="relative">
+        <h1>お問い合わせ</h1>
+        <p>
+          当サイトへのご意見やお問い合わせは下記フォームよりお願いいたします。
+          <br />
+        </p>
+        <form onKeyDown={handleFormSubmit}>
+          <InputForm
+            variant={"primary"}
+            formName="name"
+            type="text"
+            placeholder="name"
+            labelName="お名前"
+            {...register("name", {
+              required: "お名前を入力してください",
+            })}
+          />
+          {errors.name?.message && (
+            <p className="error-message">{errors.name?.message}</p>
+          )}
+          <InputForm
+            variant={"primary"}
+            formName="address"
+            type="text"
+            placeholder="address"
+            labelName="メールアドレス"
+            {...register("address", {
+              required: "アドレスを入力してください",
+            })}
+          />
+          <InputForm
+            variant={"primary"}
+            formName="subject"
+            type="text"
+            placeholder="subject"
+            labelName="タイトル"
+            {...register("subject")}
+          />
+          <TextAreaForm
+            variant={"primary"}
+            formName="main"
+            type="text"
+            labelName="本文"
+            rows={5}
+            {...register("main", {
+              required: "本文を入力してください",
+            })}
+          />
+          <Button
+            variant={"primary"}
+            className="m-5"
+            type="submit"
+            onClick={handleSubmit(onSubmit)}
+          >
+            送信
+          </Button>
+        </form>
+      </div>
+    </>
   );
 };
