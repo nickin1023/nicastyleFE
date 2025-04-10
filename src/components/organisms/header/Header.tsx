15fafba5015d69580b8f3df0762ab808a87fb55f
nickin1023 <icape0000@gmail.com>
2025-04-10 20:28:09 +0900

    fix header action

--- src/components/organisms/header/Header.tsx
+++ src/components/organisms/header/Header.tsx
@@ -1,11 +1,43 @@
 import Image from "next/image";
 import Link from "next/link";
+import { useEffect, useState } from "react";
 
 export const Header = () => {
+  const [isVisible, setIsVisible] = useState(true);
+  const [lastScrollY, setLastScrollY] = useState(0);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const currentScrollY = window.scrollY;
+
+      if (currentScrollY === 0) {
+        setIsVisible(true);
+      } else if (currentScrollY > lastScrollY && currentScrollY > 32) {
+        // 下方向にスクロールした場合はヘッダーを隠す
+        setIsVisible(false);
+      } else {
+        // 上方向にスクロールした場合はヘッダーを表示
+        setIsVisible(true);
+      }
+
+      setLastScrollY(currentScrollY);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, [lastScrollY]);
+
   return (
     <>
-      <header className="w-full bg-black text-white">
-        <div className="container mx-auto flex justify-between">
+      <header
+        className={`fixed top-0 w-full bg-black text-white transition-transform duration-300 z-50 ${
+          isVisible ? "translate-y-0" : "-translate-y-full"
+        }`}
+      >
+        <div className="container mx-auto flex justify-between p-3">
           <Link href="/">
             <Image src="/logo.png" alt="Logo icon" width={32} height={32} />
           </Link>
