b6a218b514a689904cb09e94d4320ac5369f12d2
nickin1023 <icape0000@gmail.com>
2025-04-10 20:39:00 +0900

    fix image add

--- server/api/administrator/requestGhostBase.ts
+++ server/api/administrator/requestGhostBase.ts
@@ -96,7 +96,7 @@ export const requestAdd = async <T = any>(
     posts: [
       {
         title: body.title,
-        featureImageUrl: body.featureImageUrl,
+        feature_image: body.featureImageUrl,
         html: body.html,
         status: "draft"
       }
