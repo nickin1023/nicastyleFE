bf4f22bd322735e054e7c1dd87080b9374dea9eb
nickin1023 <icape0000@gmail.com>
2025-05-11 13:56:08 +0900

    add content to footer

--- src/components/organisms/footer/Footer.tsx
+++ src/components/organisms/footer/Footer.tsx
@@ -1,9 +1,39 @@
-import React from "react";
+import Image from "next/image";
+import Link from "next/link";
 
 export const Footer = () => {
   return (
-    <footer className="w-full bg-black text-white">
-      <p>footer</p>
+    <footer className="w-full bg-[repeating-conic-gradient(#000_0_25%,#222_0_50%)] bg-[size:20px_20px] text-white flex flex-col items-center">
+      <nav className="p-4">
+        <ul className="flex justify-center">
+          <li className="px-2">
+            <Link href="/">Top</Link>
+          </li>
+          <li className="px-2">
+            <Link href="/articles">Article</Link>
+          </li>
+          <li className="px-2">
+            <Link href="/about">About</Link>
+          </li>
+          <li className="px-2">
+            <Link href="/contact">Contact</Link>
+          </li>
+        </ul>
+      </nav>
+      <div className="flex w-11/12 justify-center items-center pt-4 pb-12 border-t">
+        <Link href="/" className="p-2">
+          <Image
+            src="/logo.png"
+            alt="Logo icon"
+            width={126}
+            height={32}
+            priority={true}
+          />
+        </Link>
+        <div className="p-2">
+          <p>プライバシーポリシー</p>
+        </div>
+      </div>
     </footer>
   );
 };
