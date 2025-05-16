21dcb2f7e8dd42c603ca7ca71ab6c82eea29b69a
nickin1023 <icape0000@gmail.com>
2025-05-16 22:45:00 +0900

    fix top page content

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,6 @@
+import Image from "next/image";
 import { useEffect, useState } from "react";
+import { TOP } from "../consts/strings";
 
 export default function Home() {
   const images = [
@@ -12,6 +14,7 @@ export default function Home() {
   const [visibleCount, setVisibleCount] = useState(0);
   const [showCaret, setShowCaret] = useState(true);
 
+  // 文字の表示までの速度
   useEffect(() => {
     if (visibleCount < images.length) {
       const delay = () => {
@@ -21,7 +24,7 @@ export default function Home() {
           case images.length - 1:
             return 2000;
           default:
-            return 450;
+            return 300;
         }
       };
       const timeout = setTimeout(
@@ -32,6 +35,7 @@ export default function Home() {
     }
   }, [visibleCount]);
 
+  // タイプライターの点滅間隔
   useEffect(() => {
     const interval = setInterval(() => setShowCaret((c) => !c), 400);
     return () => clearInterval(interval);
@@ -39,7 +43,16 @@ export default function Home() {
 
   return (
     <>
-      <div className="relative aspect-[16/9] bg-gray-300">
+      <div className="relative aspect-[3/4] md:aspect-[16/8]">
+        <div className="absolute top-0 left-1 z-10 pointer-events-none w-[128px] h-[128px] md:w-[160px] md:h-[160px]">
+          <Image
+            src={"/decoration/plant1.png"}
+            alt="decoration-plant1"
+            fill
+            style={{ objectFit: "contain" }}
+            priority
+          />
+        </div>
         <div className="flex items-center justify-center h-full space-x-2 font-mono ml-4 mr-2">
           <div
             className="inline-block"
@@ -94,9 +107,26 @@ export default function Home() {
             </div>
           ))}
         </div>
+        <div className="absolute bottom-1 right-1 z-20 pointer-events-none w-[128px] h-[128px] md:w-[200px] md:h-[200px]">
+          <Image
+            src={"/decoration/plant3.png"}
+            alt="decoration-plant3"
+            fill
+            style={{ objectFit: "contain" }}
+            priority
+          />
+        </div>
       </div>
-      <div>
-        <span>top page message.</span>
+      <div className="min-h-[60vh] flex items-center justify-center p-4 mb-10">
+        <div className="max-w-full md:max-w-[80%] w-full border bg-white border-gray-200 rounded-2xl shadow-lg p-10 text-center">
+          <h1 className="text-4xl font-bold mb-6 tracking-wide">
+            {TOP.TOP_MESSAGE}
+          </h1>
+          <div className="space-y-4 text-lg text-left">
+            <p className="indent-4">{TOP.MESSAGE1}</p>
+            <p className="indent-4">{TOP.MESSAGE2}</p>
+          </div>
+        </div>
       </div>
     </>
   );
