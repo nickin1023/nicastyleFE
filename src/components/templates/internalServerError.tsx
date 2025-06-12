ac6338a5948cd92bee1102d93e891a0f7f29a19c
nickin1023 <icape0000@gmail.com>
2025-06-12 21:49:05 +0900

    fix error page design

--- src/components/templates/internalServerError.tsx
+++ src/components/templates/internalServerError.tsx
@@ -1,6 +1,6 @@
 export const InternalServerError = () => {
   return (
-    <div>
+    <div className="flex justify-center aspect-[3/4] md:aspect-[16/8] text-lg md:text-2xl items-center">
       <p>エラーが発生しました。時間を置いてから再度お試しください。</p>
     </div>
   );
--- src/components/templates/notFound.tsx
+++ src/components/templates/notFound.tsx
@@ -1,6 +1,6 @@
 export const NotFound = () => {
   return (
-    <div>
+    <div className="flex justify-center aspect-[3/4] md:aspect-[16/8] text-lg md:text-2xl items-center">
       <p>お探しのページは見つかりませんでした。</p>
     </div>
   );
